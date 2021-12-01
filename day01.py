5c2aba1be1271ce44856efe5107e77b3ed903c66
Shervin Sahba <shervin@tuta.io>
2021-12-01 12:33:14 -0800

    lambda function makes it so clean

--- day01.py
+++ day01.py
@@ -2,12 +2,9 @@ import numpy as np
 
 def main():
     depths = np.loadtxt('day01-data', dtype=int)
-    answer1 = sum(depths[:-1] < depths[1:])
-
     convolved_depths = np.convolve(depths, np.ones(3), 'valid')
-    answer2 = sum(convolved_depths[:-1] < convolved_depths[1:])
-
-    print(answer1, answer2)
+    answer = lambda x: sum(x[:-1] < x[1:])
+    print(answer(depths), answer(convolved_depths))
 
 
 if __name__ == '__main__':
