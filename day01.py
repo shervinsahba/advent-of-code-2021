c759df30cba14280e12fe65ec46f9500ad17602c
Shervin Sahba <shervin@tuta.io>
2021-12-01 12:25:07 -0800

    np.loadtxt is cleaner than python open file

--- day01.py
+++ day01.py
@@ -1,13 +1,10 @@
 import numpy as np
 
 def main():
-    with open('day01-data') as f:
-        depths = np.array(list(filter(None, f.read().split('\n'))), dtype=int)
-
+    depths = np.loadtxt('day01-data', dtype=int)
     answer1 = sum(depths[:-1] < depths[1:])
 
     convolved_depths = np.convolve(depths, np.ones(3), 'valid')
-
     answer2 = sum(convolved_depths[:-1] < convolved_depths[1:])
 
     print(answer1, answer2)
