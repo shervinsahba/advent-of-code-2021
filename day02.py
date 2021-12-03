95c8fb6bebebb275a15a5f145b034a227c8d5534
Shervin Sahba <shervin@tuta.io>
2021-12-02 17:10:22 -0800

    merged solutions 1 and 2. cleaned up

--- day02.py
+++ day02.py
@@ -1,27 +1,16 @@
 import numpy as np
 
-def answer1(directions, position = np.array([0,0])):
-    unit_vectors = {x: np.array(y) for x,y in zip(['forward', 'up', 'down'], [[1,0], [0,-1], [0,1]])}
+with open('day02-input') as f:
+    directions = list(filter(None, f.read().split()))
 
-    for name, val in zip(directions[::2], directions[1::2]):
-        position += int(val) * unit_vectors[name]
-    
-    return position
+# [horizontal, depth, aim]
+vectors = dict((['forward', 'up', 'down'], map(np.array, [[1, 1, 0], [0, 0, -1], [0, 0, 1]])))
 
+position1, position2 = [0,0,0], [0,0,0]
+for name, val in zip(directions[::2], directions[1::2]):
+    position1 += int(val) * vectors[name]
+    position2 += int(val) * np.array([1, position2[2], 1]) * vectors[name]
 
-def answer2(directions, position = np.array([0,0,0])):
-    unit_vectors = {x: np.array(y) for x,y in zip(['forward', 'up', 'down'], [[1,1,0], [0,0,-1], [0,0,1]])}
+print(position1[0]*position1[2])
+print(np.multiply(*position2[:2]))
 
-    for name, val in zip(directions[::2], directions[1::2]):
-        position += int(val) * np.array([1, position[2], 1]) * unit_vectors[name]
-    
-    return position
-
-
-if __name__ == '__main__':
-
-    with open('day02-input') as f:
-        directions = list(filter(None, f.read().split()))
-
-    print(np.multiply(*answer1(directions)))
-    print(np.multiply(*answer2(directions)[:2]))
