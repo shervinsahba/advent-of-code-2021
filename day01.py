bc36e7476ca6b64a3f253d148a5e2528e912f987
Shervin Sahba <shervin@tuta.io>
2021-12-02 19:09:55 -0800

    cleaned code

--- day01.py
+++ day01.py
@@ -1,11 +1,6 @@
 import numpy as np
 
-def main():
-    depths = np.loadtxt('day01-data', dtype=int)
-    convolved_depths = np.convolve(depths, np.ones(3), 'valid')
-    answer = lambda x: (np.diff(x) > 0).sum()
-    print(answer(depths), answer(convolved_depths))
-
-
-if __name__ == '__main__':
-    main()
+depths = np.loadtxt('day01-input', dtype=int)
+convolved_depths = np.convolve(depths, np.ones(3), 'valid')
+answer = lambda x: (np.diff(x) > 0).sum()
+print(answer(depths), answer(convolved_depths))
