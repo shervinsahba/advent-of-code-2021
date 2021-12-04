1b9baeeb91e98e461c3786bd73d8b564e3e2e313
Shervin Sahba <shervin@tuta.io>
2021-12-04 11:53:28 -0800

    slight lint and refactor

--- day03.py
+++ day03.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 def part1(data, p):  # p=0 finds most common number, p=-1 finds least common
     return int(''.join([Counter(x).most_common()[p][0] for x in zip(*data)]), 2)
 
@@ -8,7 +9,7 @@ def part2(data, p):
     for n in range(12):
         counts = Counter(list(zip(*data))[n]).most_common()
         c = str(1 + p) if (len(counts) == 2 and counts[0][1] == counts[1][1]) else counts[p][0]
-        data = list(filter(lambda x: x[n]==c, data))
+        data = list(filter(lambda x: x[n] == c, data))
     return int(''.join(data[0]), 2)
 
 
--- day04.py
+++ day04.py
@@ -1,29 +1,28 @@
 import numpy as np
 
-def prep_data():
-    with open('input/day04') as f:
-        calls, *boards = f.read().split('\n')
-
-    calls = np.array(calls.split(','), int)
-    boards = np.loadtxt(boards, int).reshape(-1,5,5)
-    return boards, calls
-
 
 def bingo(boards, calls):
     winners, scores = [], []
-    boards_ = np.copy(boards)    
+    boards_ = np.copy(boards)
     for call in calls:
         for n, board in enumerate(boards_):
             if n not in winners and call in board:
-                r, c = np.where(board==call)
-                board[r,c] = -1
-                if (board[r,:] < 0).all() or (board[:,c] < 0).all():
-                    scores.append(board[board>0].sum() * call)
+                r, c = np.where(board == call)
+                board[r, c] = -1
+                if (board[r, :] < 0).all() or (board[:, c] < 0).all():
+                    scores.append(board[board > 0].sum() * call)
                     winners.append(n)
     return list(zip(winners, scores))
 
 
-results = bingo(*prep_data())
+if __name__ == '__main__':
+    with open('input/day04') as f:
+        calls, *boards = f.read().split('\n')
 
-print('bingo:', results[0])
-print('bongo:', results[-1])
+    calls = np.array(calls.split(','), int)
+    boards = np.loadtxt(boards, int).reshape(-1, 5, 5)
+
+    results = bingo(boards, calls)
+
+    print('bingo:', results[0])
+    print('bongo:', results[-1])
