3eb76001d0453f80e337a115c5e6a3b7dd558472
Shervin Sahba <shervin@tuta.io>
2021-12-05 23:05:59 -0800

    better var names

--- day06.py
+++ day06.py
@@ -1,12 +1,12 @@
 with open('input/day06') as f:
     data = list(map(int, f.read().split(',')))
 
-data = [data.count(timer) for timer in range(9)]
+counts = [data.count(timer) for timer in range(9)]
 
-def tally(data, days):
+def total(c, days):
     for _ in range(days):
-        data = data[1:] + [data[0]]
-        data[6] += data[-1]
-    return sum(data)
+        c = c[1:] + [c[0]]
+        c[6] += c[-1]
+    return sum(c)
 
-print(tally(data, 80), tally(data, 256))
+print(total(counts, 80), total(counts, 256))
