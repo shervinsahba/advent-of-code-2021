573f3989b2fe5684587967105636d00ba361021c
Shervin Sahba <shervin@tuta.io>
2021-12-05 22:49:16 -0800

    cleaned up, removed another line

--- day06.py
+++ day06.py
@@ -1,14 +1,12 @@
 with open('input/day06') as f:
     data = list(map(int, f.read().split(',')))
-    
+
 data = [data.count(timer) for timer in range(9)]
 
-total = []
-for _ in range(256):    
-    data.append(data[0])
-    data = [x for x in data[1:]]
-    data[6] += data[-1]
+def tally(data, days):
+    for _ in range(days):
+        data = data[1:] + [data[0]]
+        data[6] += data[-1]
+    return sum(data)
 
-    total.append(sum(data))
-   
-print(total[79], total[255])
+print(tally(data, 80), tally(data, 256))
