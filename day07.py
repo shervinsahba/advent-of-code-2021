c92930badd9cc68c25b834b62c0ca473bd03ee9e
Shervin Sahba <shervin@tuta.io>
2021-12-07 11:16:20 -0800

    only need to consider ceil and floor of mean

--- day07.py
+++ day07.py
@@ -2,7 +2,7 @@ import numpy as np
 
 data = np.loadtxt('input/day07', int, delimiter=',')
 
-# 1. brute force solutions.
+# 1. brute force solutions for both parts
 gas = min(np.abs(data - x).sum() for x in range(np.max(data)))
 
 cost = lambda s: s*(s+1)/2  # sum of values 1 to s
@@ -18,8 +18,8 @@ gas = np.abs(data - np.median(data)).sum()
 
 # in part 2, the cost is given by sum of 1 to s, which is also the average of the L2 and L1 norms.
 # Minimizing the cost gives s = mean(x) - 1/2 * mean(sgn(x-s)), which is ugly.
-# But we're working with integers, so we know s is within an integer of mean(x).
-xb = np.round(np.mean(data)) + np.array([-1,0,1])
-gas2 = min(cost(np.abs(data - x)).sum() for x in xb)
+# But we're working with integers, so we know s is within 1/2 of mean(x).
+avg = np.mean(data)
+gas2 = min(cost(np.abs(data - x)).sum() for x in [np.floor(avg), np.ceil(avg)])
 
 print(gas, gas2)
